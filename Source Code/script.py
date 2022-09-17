5f518b099b2e23122c565be56fb4cd0a35211557
Hamza Asif <hamzaasif0987@gmail.com>
2022-09-18 03:36:00 +0500

    made download files function

--- Source Code/script.py
+++ Source Code/script.py
@@ -1,40 +1,43 @@
-from lib2to3.pgen2 import driver
+import time
 from selenium import webdriver
+from lib2to3.pgen2 import driver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import WebDriverException
-import time
-
-chrome_executable = Service(executable_path='chromedriver.exe', log_path='NUL')
-
-options = webdriver.ChromeOptions()
-prefs = {"download.default_directory": "E:\Freelance\XML Data handling\Source Code\data_files"}
-options.add_experimental_option("prefs", prefs)
-
-driver = webdriver.Chrome(service=chrome_executable, chrome_options=options)
-
-driver.implicitly_wait(500)  # gives an implicit wait for 20 seconds
-
-driver.get('https://www.handelsregister.de/rp_web/erweitertesuche.xhtml')
 
 
-search_bar = driver.find_element(
-    by=By.XPATH, value="//textarea[@id='form:schlagwoerter']").send_keys("faraz")
+
+def Search_And_Download_Files():
+    chrome_executable = Service(executable_path='chromedriver.exe', log_path='NUL')
+
+    options = webdriver.ChromeOptions()
+    prefs = {"download.default_directory": "E:\Freelance\XML Data handling\Source Code\data_files"}
+    options.add_experimental_option("prefs", prefs)
+
+    driver = webdriver.Chrome(service=chrome_executable, chrome_options=options)
+
+    driver.implicitly_wait(500)  # gives an implicit wait for 20 seconds
+
+    driver.get('https://www.handelsregister.de/rp_web/erweitertesuche.xhtml')
 
 
-find_button = search_bar = driver.find_element(
-    by=By.XPATH, value="//button[@id='form:btnSuche']")
-find_button.click()
+    search_bar = driver.find_element(
+        by=By.XPATH, value="//textarea[@id='form:schlagwoerter']").send_keys("faraz")
 
-SI_data = driver.find_element(
-    by=By.XPATH, value="//a[6]/span[contains(text(),'SI')]")
 
-try:
-    SI_data.click()
-    download_button = driver.find_element(
-        by=By.XPATH, value="//button[@id='form:kostenpflichtigabrufen']")
-    download_button.click()
-except WebDriverException:
-    print("No element found")
+    find_button = driver.find_element(
+        by=By.XPATH, value="//button[@id='form:btnSuche']")
+    find_button.click()
 
-time.sleep(10)
+    SI_data = driver.find_element(
+        by=By.XPATH, value="//a[6]/span[contains(text(),'SI')]")
+
+    try:
+        SI_data.click()
+        download_button = driver.find_element(
+            by=By.XPATH, value="//button[@id='form:kostenpflichtigabrufen']")
+        download_button.click()
+    except WebDriverException:
+        print("No element found")
+
+    time.sleep(10)
