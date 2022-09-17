9a991e94776158f276504e7148e3175b9779f2e9
Hamza Asif <hamzaasif0987@gmail.com>
2022-09-18 03:27:30 +0500

    automated files downloading using selenium

---
+++ Source Code/script.py
@@ -0,0 +1,40 @@
+from lib2to3.pgen2 import driver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import WebDriverException
+import time
+
+chrome_executable = Service(executable_path='chromedriver.exe', log_path='NUL')
+
+options = webdriver.ChromeOptions()
+prefs = {"download.default_directory": "E:\Freelance\XML Data handling\Source Code\data_files"}
+options.add_experimental_option("prefs", prefs)
+
+driver = webdriver.Chrome(service=chrome_executable, chrome_options=options)
+
+driver.implicitly_wait(500)  # gives an implicit wait for 20 seconds
+
+driver.get('https://www.handelsregister.de/rp_web/erweitertesuche.xhtml')
+
+
+search_bar = driver.find_element(
+    by=By.XPATH, value="//textarea[@id='form:schlagwoerter']").send_keys("faraz")
+
+
+find_button = search_bar = driver.find_element(
+    by=By.XPATH, value="//button[@id='form:btnSuche']")
+find_button.click()
+
+SI_data = driver.find_element(
+    by=By.XPATH, value="//a[6]/span[contains(text(),'SI')]")
+
+try:
+    SI_data.click()
+    download_button = driver.find_element(
+        by=By.XPATH, value="//button[@id='form:kostenpflichtigabrufen']")
+    download_button.click()
+except WebDriverException:
+    print("No element found")
+
+time.sleep(10)
